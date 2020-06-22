299a0844b9875d1ba37597c22fde08a7502cee42
Nisha Thapa <nishathapa4727@gmail.com>
2020-06-23 00:29:09 +0530

    lyrics page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import {Provider} from './context';
 
 import Navbar from './components/layout/Navbar';
 import Index from './components/layout/Index';
+import Lyrics from './components/tracks/Lyrics';
+
 
 import './App.css';
 
@@ -18,6 +20,8 @@ function App() {
         <div className="container">
           <Switch>
             <Route exact path="/" component={Index} />
+            <Route exact path="/lyrics/track/:id" component={Lyrics} />
+
           </Switch>
         </div>
       </React.Fragment>  
--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import spinner from './spinner.gif';
+import Spinner from './Spinner.gif';
 
 export default () => {
     return (
         <div>
             <img
-            src={spinner}
+            src={Spinner}
             alt="Loading..."
             style={{width: '200px', margin: '40px auto', display: 'block'}}
             />
